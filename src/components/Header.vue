bc3da0c985da84d9f2987db36535c11ceef5f74a
yeseul <from.yeseul@gmail.com>
2024-07-18 14:57:21 +0900

    마무리

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,27 @@
 <script setup>
-    import {headerNav} from '../constants';
+import { headerNav } from "@/constants/index";
 </script>
+
 <template>
-    <header id="header" role="banner">
+    <header id="header" role="heading">
         <div class="header__inner">
             <h1 class="header__logo">
                 <a href="#">portfolio<em>vue.js</em></a>
             </h1>
-            <nav 
-                class="header__nav"
-                :class="{show: isNavVisible}" 
-                role="navigation" 
-                aria-label="메인 메뉴"
-            >
+            <nav class="header__nav" role="navigation" aria-label="메인 메뉴" :class="{ show: isNavVisible }">
                 <ul>
-                    <!-- <li><a href="#intro">intro</a></li>
-                    <li><a href="#skill">skill</a></li>
-                    <li><a href="#site">site</a></li>
-                    <li><a href="#port">portfolio</a></li>
-                    <li><a href="#contact">contact</a></li> -->
                     <li v-for="(nav, key) in headerNav" :key="key">
-                        <a href="#intro">{{ nav.title }}</a>
-                    </li>
-                </ul>
+                        <a :href="nav.url" @click="scrollLink($event)">{{ nav.title }}</a>
+                    </li> 
+                </ul> 
             </nav>
-            <div 
-                class="header__nav__mobile" 
-                id="headerToggle" 
-                aria-controls="primary-menu" 
-                :aria-expanded="isNavVisible.toString()" 
-                role="button" 
-                tabindex="0"
+            <div
+                class="header__nav__mobile"
+                id="headerToggle"
+                aria-controls="primary-menu"
+                :aria-expanded="isNavVisible.toString()"
                 @click="toggleMobileMenu"
-            >
+                >
                 <span></span>
             </div>
         </div>
@@ -40,18 +29,28 @@
 </template>
 
 <script>
-    export default {
-        data(){
-            return {
-                isNavVisible: false
+export default {
+    data() {
+        return {
+            isNavVisible: false,
+        };
+    },
+    methods: {
+        toggleMobileMenu() {
+            this.isNavVisible = !this.isNavVisible;
+        },
+        scrollLink(event) {
+            event.preventDefault();
+
+            const targetId = event.target.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
             }
         },
-        methods: {
-            toggleMobileMenu(){
-                this.isNavVisible = !this.isNavVisible;
-            }
-        }
-    }
+    },
+};
 </script>
 
 <style lang="scss" scoped>
